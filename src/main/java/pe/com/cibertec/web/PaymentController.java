8b078abb4e77d50434ef0d4f550b9c0418932d84
David Gallessi <you@example.com>
2023-04-04 05:33:21 -0500

    Add Class PaymentController to project

---
+++ src/main/java/pe/com/cibertec/web/PaymentController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.com.cibertec.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author digal
+ */
+@RestController
+public class PaymentController {
+    
+}
