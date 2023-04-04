8f830c2b2e1af7136830ecb21f3f304ecb82c573
David Gallessi <you@example.com>
2023-04-03 22:22:42 -0500

    Se agrega la clase empleado

---
+++ src/main/java/pe/com/cibertec/domain/Empleado.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.com.cibertec.domain;
+
+import javax.persistence.Entity;
+import lombok.Data;
+
+/**
+ *
+ * @author digal
+ */
+@Entity
+@Data
+public class Empleado {
+    
+}
